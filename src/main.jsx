356861882aededc0b884e7ec2ec59a0bfff5db18
Elliot Henriquez <elliothenriq@gmail.com>
2023-05-14 13:27:37 -0500

    v1.0 Initial Commit

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,13 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App'
-// import Schedule from './Schedule'
 import Events from './Events'
-// import Test from './Test'
 import theme from './theme';
-// import "./index.css";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <ChakraProvider theme={theme}>
-      {/* <Schedule /> */}
+
       <Events />
-      {/* <Test /> */}
+
     </ChakraProvider>
 )
--- src/theme.js
+++ src/theme.js
@@ -1,16 +1,6 @@
 import { extendTheme } from '@chakra-ui/react';
 
 const theme = extendTheme({
-//   fonts: {
-//     heading: `'Syne', sans-serif`,
-//     body: `'Plus Jakarta Sans', sans-serif`,
-//     },
-//   colors: {
-//     celest: "#3699CF",
-//     indigo: "#024675",
-//     pumpkin: "#FE7F2D",
-//     sunglow: "#FCCA46"
-//   }
 
 });
 
